c3dfea9e3da02c834a0a74ea63383e3d4e34bdbd
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2023-01-10 20:55:52 -0700

    uifdgdf

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.use("/", htmlRoutes);
-app.use("/api", apiRoutes);
+app.use("/api/notes", apiRoutes);
 app.get("*", function(req, res) {
     
     res.sendFile(path.join(__dirname, "../public/index.html"));
